9b4236c18caf495ce27306e14ee5276f9b8ef218
gmldudrkd <gmldud8100@naver.com>
2023-04-12 01:51:34 +0900

    DB 쿼리 로깅 추가

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,6 +18,8 @@ public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
     @Test
+    @Transactional //테스트 끝난 후 롤백되어 데이터 없어짐
+    @Rollback(value = false) //테스트 끝난 후 롤백되어 데이터 없어지지 않게
     public void testMember(){
         //given
         Member member = new Member();
@@ -29,5 +32,6 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); //영속성 context에 entitiy는 같은 트랜잭션 내에 동일하게 취급,,,,,?
     }
 }
